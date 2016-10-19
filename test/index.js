d9127145a35c5f6458a5f50d41a7c2a92da055ab
wangshijun2010 <wangshijun2010@gmail.com>
2016-10-19 10:30:10 +0800

    code formatting

--- test/index.js
+++ test/index.js
@@ -2,7 +2,7 @@ import assert from 'assert';
 import expressJwtSign from '../lib';
 
 describe('express-jwt-sign', function () {
-  it('should have unit test!', function () {
-    assert(false, 'we expected this package author to add actual unit tests.');
-  });
+    it('should have unit test!', function () {
+        assert(false, 'we expected this package author to add actual unit tests.');
+    });
 });
